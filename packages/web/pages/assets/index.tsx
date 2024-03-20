573ea3995ec6d11251b98fe0937eb9c3b06fbfa2
bterpinc <borut.terpinc@kalmia.si>
2024-03-20 09:12:07 +0100

    WIP

--- packages/web/pages/assets/index.tsx
+++ packages/web/pages/assets/index.tsx
@@ -3,20 +3,20 @@ import { NextSeo } from 'next-seo';
 
 import { AssetsPageV2 } from '~/components/complex/assets-page-v2';
 import { useTranslation } from '~/hooks';
-import { useFeatureFlags } from '~/hooks/use-feature-flags';
+// import { useFeatureFlags } from '~/hooks/use-feature-flags';
 
 const Assets: NextPage = () => {
-  const flags = useFeatureFlags();
+  // const flags = useFeatureFlags();
   const { t } = useTranslation();
 
   // will lose SSR until we delete the old assets page and FF
-  if (!flags._isInitialized)
+  /* if (!flags._isInitialized)
     return (
       <NextSeo
         title={t('seo.assets.title')}
         description={t('seo.assets.description')}
       />
-    );
+    ); */
 
   return (
     <>
